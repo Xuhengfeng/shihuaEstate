039af40c9ba3e91d4de9895f2655910bf2d07a9a
564297479@qq.com <564297479@qq.com>
2018-04-22 15:46:14 +0800

    Xuhengfeng

--- src/components/mapSearch/mapSearch.vue
+++ src/components/mapSearch/mapSearch.vue
@@ -96,65 +96,67 @@ export default {
   data() {
     return {
       map: null,//地图实例
-      areaList: {//地图找房
-        arerDetails: [],//区域
-        districtDetails: [],//片区
-        housingDetails: [],//小区
-      },
+      usedAreaDetails: [],//使用房源(区域 片区 小区)tree数据
+      rentAreaDetails: [],//租房房源
+
       inputKeyword: '',//输入关键词
       isShowSide: true,//是否显示side
-      polygon: '',//
-      svg: '',//svg
-      path: '',//路径
-      circleNum: 1,//开始画圈找房 取消
-      metroNum: 1,//开始地铁找房 取消
-      pencil: '../../../static/pencil.ico',
-      selectCity: JSON.parse(localStorage.selectCity),//当前选择的城市
+      polygon: '',//绘图
+      circleNum: 1,//开始画圈找房1 取消画圈找房2
+      metroNum: 1,//开始地铁找房1 取消地铁找房2
+      pencil: '../imgs/home/pencil.ico',
+      selectCity: JSON.parse(localStorage.selectCity),
+      //二手房 租房 小区
+      IPS: [this.$url.URL.MAPHOUSEALLTREE, this.$url.URL.MAPHOUSEALLTREE, this.$url.URL.MAPHOUSEALLTREE],
     }
   },
   created() {
-    this.requestAllCityData();
     //创建地址解析器实例
     let myGeo = new BMap.Geocoder();
-        myGeo.getPoint(this.selectCity.name, (point)=> {
-          this.initMap(point.lng, point.lat);//初始化百度地图
-        })
+    myGeo.getPoint(this.selectCity.name, (point)=> {
+      this.initMap(point.lng, point.lat);//初始化地图(中心) 
+    })
+    //请求所有数据
+    this.requestAllCityData();
   },
   methods: {
+    //显示房源列表
     isShowHouseList() {
       this.isShowSide?this.$refs.side.style.left="0":this.$refs.side.style.left="-438px";
       this.isShowSide = !this.isShowSide
     },
-    //区域级别房源
+    //区域级别房源 二手房 租房 小区
     requestAllCityData() {
-      console.log(this.$route.params.houseTypeId)
-      if(this.$route.params.houseTypeId == 11) {
-        //二手房区域级别房源
-        this.$http.post(this.$url.URL.MAPHOUSEALLTREE, {
-          scity: this.selectCity.value
-        })
-        .then(res=> {
-          this.areaList = res.data.data.usedAreaDetails;
-
-          //初始化区域级别数据显示
-          this.areaList.forEach((item)=>{
-              let point = new BMap.Point(item.px, item.py);
-              this.addLabel(point, item);
-          })
-        
-          
-        })
-        .catch(error => {
-          console.log(error);
-        });
+      let houseType = this.$route.params.houseTypeId;
+      let num;
+      if(houseType == 11) {
+        num = 0;
+      }else if(houseType == 22) {
+        num = 1;
+      }else if(houseType == 33) {
+        num = 2;
       }
+      this.$http.post(this.IPS[num], {
+        scity: this.selectCity.value
+      })
+      .then(res=> {
+        this.usedAreaDetails = res.data.data.usedAreaDetails;
+        this.rentAreaDetails = res.data.data.rentAreaDetails;
+        //初始化区域级别数据显示
+        this.usedAreaDetails.forEach((item)=>{
+            let point = new BMap.Point(item.px, item.py);
+            this.addLabel(point, item);
+        })
+      })
     },
     //这里百度地图start------------------------------------------------------------------------<<<<<<<<<<
+    //初始化地图
     initMap(px, py) {
-      this.createMap(px, py); //创建地图
-      this.setMapEvent(); //设置地图事件
+      this.createMap(px, py);
+      this.setMapEvent();
     },
-    createMap(lng, lat) {//创建地图
+    //创建地图
+    createMap(lng, lat) {
       //11 12      区域级别
       //13 14      片区级别
       //15 16      小区级别
@@ -165,7 +167,8 @@ export default {
       let pt = new BMap.Point(lng, lat);//初始时候 首先获取到目的城市的坐标 例如: 北海站坐标109.134582,21.459389
       this.map.centerAndZoom(pt, 12);//初始中心点和缩放比例
     },
-    addLabel(point, obj){//创建label 
+    //创建地图label 
+    addLabel(point, obj){
         //判断当前是处于什么zoom 级别的;
         let currentZoom = this.map.getZoom();
 
@@ -198,7 +201,7 @@ export default {
         }
         
 
-        var label = new BMap.Label(content, {
+        let label = new BMap.Label(content, {
             offset: new BMap.Size(-40,-40),
             position: point //指定文本标注所在的地理位置               
           })
@@ -230,9 +233,9 @@ export default {
           this.$refs.side.style.left="0";
         }
       }) 
-
     },
-    setMapEvent() {//设置地图事件
+    //设置地图事件
+    setMapEvent() {
       this.map.enableScrollWheelZoom();
       this.map.enableKeyboard();
       this.map.enableDragging();
@@ -241,7 +244,8 @@ export default {
     //这里百度地图end------------------------------------------------------------------<<<<<<<<<<
 
     //画圈找房start--------------------------------------------------------------------<<<<<<<<<<
-    changeSvg() {//修改svg
+    //修改svg
+    changeSvg() {
       const style = "position:absolute;top:-500px;left:-500px;width:2536px;height:1281px";
       this.svg = d3.select("svg")
                 .attr("x", "2536px")
@@ -252,7 +256,8 @@ export default {
                 .attr("style", style)
                 .attr("fill", "rgba(0,0,0,.3)");
     },
-		getMouse(e){//获取坐标
+    //获取坐标
+		getMouse(e){
       var e=e||window.event;
       var mouse={x:0,y:0};
 			// mouse.x=e.pageX+this.amendment().newLeft;
@@ -261,7 +266,8 @@ export default {
       mouse.y=e.pageY-111;
 			return mouse;
     },
-    amendment() { //修正鼠标位置参数
+    //修正鼠标位置参数
+    amendment() { 
       let str1 = document.querySelector(".BMap_mask").getAttribute("style").split(";")[1];//left
       let str2 = document.querySelector(".BMap_mask").getAttribute("style").split(';')[2];//top
           str1 = str1.replace(/[^-?\d+]/ig," ");
@@ -273,85 +279,110 @@ export default {
         newTop: newTop
       }
     },
-    beginDraw() {//点击画图按钮 开始触发这个函数
+    //点击画图按钮进入绘图
+    beginDraw() {
+      //初始化绘图
+      //清空覆盖物
+      this.map.clearOverlays(); 
       this.polygon = new BMap.Polygon(); 
-      this.map.addOverlay(this.polygon);  
+      this.map.addOverlay(this.polygon);
     
       //鼠标样式替换
+      console.log(document.querySelector("svg"))
       document.querySelector("svg").style.cursor = 'url('+this.pencil+'),default';
       this.circleNum = 2;
       this.map.disableDragging();//设置地图禁止拖拽   
-      var mapNode = document.getElementById('map');
-      var str='';
-      var x1,y1;//起始点坐标
-      var x2,y2;//结束点坐标
-      var xyArr = '';//point数组
+
+      let mapNode = document.getElementById('map');
+      let str='';
+      let x1,y1;//起始点坐标
+      let x2,y2;//结束点坐标
+      let xyArr = '';//point数组
       mapNode.onmousedown = (e)=>{//开始画圈圈
         x1 =  this.getMouse(e).x;
         y1 =  this.getMouse(e).y;
-        
         mapNode.onmousemove = (e)=>{
             x2 =  this.getMouse(e).x;
             y2 =  this.getMouse(e).y;
-            //像素坐标转地理坐标
-            var p=new BMap.Pixel(x2,y2);  
-            var point1=this.map.pixelToPoint(p);  
-            var positionArray=new Array(point1.lng,point1.lat);   
+            //像素坐标转地理坐标(坐标同步)
+            let p=new BMap.Pixel(x2,y2);  
+            let point1=this.map.pixelToPoint(p);  
+            let positionArray=new Array(point1.lng,point1.lat);   
                 xyArr += positionArray[0]+", "+positionArray[1]+";"
                 this.polygon.setPath(xyArr);
         }
       }
       mapNode.onmouseup = ()=>{//结束
-        // this.map.setViewport(xyArr);    //调整视野
-        document.querySelector("svg").style.cursor = '';
+        // this.map.setViewport(xyArr);//调整视野
         this.map.enableDragging();//设置地图打开拖拽
         mapNode.onmousemove = null;
         mapNode.onmousedown = null;
         mapNode.onmouseup = null;
+        document.querySelector("svg").style.cursor = '';
       }
     },
     
     exitDraw() {// 结束画圈
       this.map.clearOverlays();   
       this.circleNum = 1;
-    }
+      this.renderViewArea();
+    },
+
     //画圈找房end---------------------------------------------------------------------------<<<
 
+    //地图可视区域的房源数据
+    viewArea() {
+      this.map.addEventListener("dragend", ()=>{//拖动结束
+        var bs = this.map.getBounds();   //获取可视区域
+        var bssw = bs.getSouthWest();   //可视区域左下角
+        var bsne = bs.getNorthEast();   //可视区域右上角
+        console.log(bssw, bsne)
+        //tode 请求区域数据
+      })
+    },
+    renderViewArea() {
+      //清空覆盖物
+      this.map.clearOverlays();    
+      let currentZoom = this.map.getZoom();
+      if(currentZoom<=12) {//区域级别
+        //区域级别数据显示
+        this.usedAreaDetails.forEach((item)=>{
+          let point = new BMap.Point(item.px, item.py);
+          this.addLabel(point, item);
+        })
+      }else if(currentZoom>12&&currentZoom<=14) {//片区级别
+        //片区级别数据显示
+        this.usedAreaDetails.forEach((item)=>{
+          item.usedDistrictDetails.forEach((item2)=>{
+            let point = new BMap.Point(item2.px, item2.py);
+            this.addLabel(point, item2);
+          })
+        })
+      }else if(currentZoom>=15) {//小区级别
+        //小区级别数据显示
+        this.usedAreaDetails.forEach((item)=>{
+          item.usedDistrictDetails.forEach((item2)=>{
+            item2.usedHousingDetails.forEach((item3)=>{
+              let point = new BMap.Point(item3.px, item3.py);
+              this.addLabel(point, item3);
+            })
+          })
+        })
+      }
+    }
   },
   mounted() {
     this.changeSvg();  //修改svg
     setTimeout(()=> {
       this.map.addEventListener("zoomend", ()=>{
-        //清空覆盖物
-        this.map.clearOverlays();    
+        if(this.circleNum == 2){
+          //绘图状态
 
-        let currentZoom = this.map.getZoom();
-        if(currentZoom<=12) {//区域级别
-          //区域级别数据显示
-         this.areaList.arerDetails.forEach((item)=>{
-            let point = new BMap.Point(item.px, item.py);
-            this.addLabel(point, item);
-          })
-        }else if(currentZoom>12&&currentZoom<=14) {//片区级别
-          //片区级别数据显示
-          this.areaList.districtDetails.forEach((item)=>{
-            let point = new BMap.Point(item.px, item.py);
-            this.addLabel(point, item);
-          })
-        }else if(currentZoom>15) {//小区级别
-          //小区级别数据显示
-          this.areaList.housingDetails.forEach((item)=>{
-            let point = new BMap.Point(item.px, item.py);
-            this.addLabel(point, item);
-          })
+        }else if(this.circleNum == 1){
+          //未绘图状态
+          this.renderViewArea();
         }
       });
-      this.map.addEventListener("dragend", ()=>{//拖动结束  重新改变svg位置 使其和地图同步
-        var bs = this.map.getBounds();   //获取可视区域
-        var bssw = bs.getSouthWest();   //可视区域左下角
-        var bsne = bs.getNorthEast();   //可视区域右上角
-        console.log(bssw, bsne)
-      })
     },1000)
    
   }
