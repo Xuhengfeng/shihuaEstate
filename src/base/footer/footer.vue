ec156542fcef40387c6712fa11478fe67c6bb8af
Xuhengfeng <564297479@qq.com>
2018-05-10 22:44:36 +0800

    添加网站的ico 图标

--- src/base/footer/footer.vue
+++ src/base/footer/footer.vue
@@ -55,11 +55,11 @@
 <script>
 </script>
 
-<style>
+<style scoped>
 .footer{
 	width: 100%;
 	height: 380px;
-	background-color: #F5F5F5;
+	background-color: #000;
 	background-image: url(../../imgs/home/di.png);
 	background-repeat: no-repeat;
 	background-position: center 0;
