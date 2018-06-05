b04ec24df4cff24fa6dc657091deb26241c45d94
xuhengfeng <564297479@qq.com>
2018-06-05 14:15:27 +0800

    update

--- src/components/mine/indexeditInfo.vue
+++ src/components/mine/indexeditInfo.vue
@@ -1,8 +1,48 @@
 <template>
+    <div>
+        <header>
+            <div class="title">我的账户信息</div>
+            <ul> 
+                <li v-for="item in tabs">{{item}}</li>
+            </ul>
+        </header>
+        <section>
+            
+        </section>
+    </div>
 </template>
 
 <script>
+    export default {
+        data() {
+            return {
+                tabs: ['上传头像','修改昵称','修改密码'],
+            }
+        },
+
+    }
 </script>
 
-<style>
+<style lang="less" scoped>
+header{
+    .title{
+        height: 120px;
+        line-height: 120px;
+        font-size: 30px;
+        color: #000000;
+    }
+    ul{
+        background: #e5e5e5;
+        height: 40px;
+        line-height: 40px;
+        li{
+            float: left;
+            color:#000000;
+        }
+    }
+}
+// 高亮
+.bgcolor{
+    background: #d0d0d1;
+}
 </style>
