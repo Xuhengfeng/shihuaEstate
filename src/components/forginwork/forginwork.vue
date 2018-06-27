c84a7efe178707176382a8bd122d0b4daff10eaa
huangdanni <huangdanni@zhuotaiju.com>
2018-06-27 18:32:26 +0800

    update

---
+++ src/components/forginwork/forginwork.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="home">
+        <div class="header"></div>
+        <div class="message">
+            <ul>
+                <li><img src="../../imgs/home/qidai.png"/></li>
+                <li>商务合作<span style="margin-left:10px;">欢迎咨询</span></li>
+                <li>电话：0771-5652566</li>
+                <li>http://www.shihua365.cn</li>
+                <li>南宁市青秀区懂葛路118号青秀万达写字楼西二栋47层</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.header {
+    width: 100%;
+    height: 480px;
+    // background-color: #F5F5F5;
+    background-image: url(../../imgs/home/zhuone.png);
+    background-repeat: no-repeat;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+}
+.message{
+    text-align: center;
+    
+}
+.message>ul{
+    padding: 96px 0 106px 0;
+}
+.message>ul>li:nth-child(2){
+    padding:60px 0 50px 0;
+    color: #9d6224;
+    font-size: 36px;
+}
+.message>ul>li:nth-child(3){
+    padding:0 0 20px 0;
+    color: #9d6224;
+    font-size: 18px;
+}
+.message>ul>li:nth-child(4){
+    padding:0 0 20px 0;
+    color: #9d6224;
+    font-size: 18px;
+}
+.message>ul>li:nth-child(5){
+    padding:0 0 20px 0;
+    color: #9d6224;
+    font-size: 18px;
+}
+</style>
--- src/components/more/housetuoguan.vue
+++ src/components/more/housetuoguan.vue
@@ -19,7 +19,40 @@
                     <div class="title">我们的优势</div>
                     <div class="text">世华托管依托广西世华易居房地产代理有限公司的规模优势，为客户构建全方位的管家体系，全面解决业主的房屋租赁的后顾之忧</div>
                     <ul>
-                        <li v-for="item in cleanimg" class="fl"><img :src="item.imgUrl"/></li>
+                        <li v-for="item in cleanimgtwo" class="fl"><img :src="item.imgUrl"/></li>
+                    </ul>
+                     <ul>
+                        <li v-for="item in txtone" class="fl txtone">{{item.contenttxt}}</li>
+                    </ul>
+                    <ul>
+                        <li v-for="item in txt" class="fl txt">{{item.content}}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="section bgColor1">
+            <div class="container">
+                <div class="clean">
+                    <div class="title">托管流程</div>
+                    <div class="text">只需四步，世华托管轻松帮您搞定房屋委托</div>
+                    <ul>
+                        <li v-for="item in cleanimgthree" class="fl txttwo"><img :src="item.imgUrl"/></li>
+                    </ul>
+                    <ul>
+                        <li v-for="item in txttwo" class="fl order">{{item.contenttxt}}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+         <div class="section bgColor1">
+            <div class="container">
+                <div class="clean">
+                    <ul class="message">
+                        <li class="fl">北海业务中心：0779-6889999</li>
+                        <li>南宁业务中心：17344208467  尹先生</li>
                     </ul>
                 </div>
             </div>
@@ -36,9 +69,21 @@ export default {
                 {imgUrl:require("../../imgs/home/gongchenbu.png")},
                 {imgUrl:require("../../imgs/home/yewubu.png")}],
             cleanimgtwo:[
-                {imgUrl:require("../../imgs/home/baojiebu.png")},
-                {imgUrl:require("../../imgs/home/gongchenbu.png")},
-                {imgUrl:require("../../imgs/home/yewubu.png")}]
+                {imgUrl:require("../../imgs/home/one.png")},
+                {imgUrl:require("../../imgs/home/two.png")},
+                {imgUrl:require("../../imgs/home/three.png")}],
+            cleanimgthree:[
+                {imgUrl:require("../../imgs/home/tianxie.png")},
+                {imgUrl:require("../../imgs/home/order.png")},
+                {imgUrl:require("../../imgs/home/tijiao.png")},
+                {imgUrl:require("../../imgs/home/qianyue.png")}],
+            txt:[
+                {content:" 所有琐碎事情均由托管中心代处理，让房屋得到更好的爱护，使房子增值保值 "},
+                {content:" 托管流程严谨规范，收益有保障，规避租金无法及时收取的风险 "},
+                {content:" 房东不需要为和租客纠纷而烦恼；无论房东在何地都无须担忧 "},
+             ],
+             txtone:[{contenttxt:"品质服务"},{contenttxt:"收益稳健"},{contenttxt:"省心省力"},],
+             txttwo:[{contenttxt:"提交委托信息"},{contenttxt:"预约看房时间"},{contenttxt:"制定出租方案"},{contenttxt:"签订托管合同 "}]
 		}
 	},
 }
@@ -51,9 +96,6 @@ export default {
     background-color: #F5F5F5;
     background-image: url(../../imgs/home/housetuoguan.png);
     background-repeat: no-repeat;
-    background-position: center 0;
-    background-attachment: fixed;
-    background-size: 1920px 717px;
     -webkit-box-sizing: border-box;
     box-sizing: border-box;
 }
@@ -83,7 +125,39 @@ export default {
     height: 100%;
     padding-bottom: 50px;
 }
-.clean>ul>li{
+.clean:nth-child(1) li{
     padding-left: 55px;
 }
+.txtone{
+    font-size: 24px;
+    color: #333333;
+    margin-left: 100px;
+    width: 205px;
+    padding: 26px 0 16px 0;
+}
+.txttwo{
+    width: 130px;
+    margin-left: 83px;
+}
+.order{
+    width: 150px;
+    padding-top:20px;
+    color: #333333;
+    font-size: 24px;
+    margin-left: 55px;
+}
+.txt{
+    width: 304px;
+    height: 44px;
+    text-align: center;
+    line-height:25px;
+}
+.message{
+    padding-bottom: 60px;
+}
+.message li{
+    margin-left: 170px;
+    text-align: center;
+}
+
 </style>
