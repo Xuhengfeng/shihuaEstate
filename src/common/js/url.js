03d88e7d8a3d5007093addbcbf0cb8a46de9b3d6
xuhengfeng <564297479@qq.com>
2018-07-03 18:25:23 +0800

    update

--- src/common/js/url.js
+++ src/common/js/url.js
@@ -1,7 +1,7 @@
-let localhostDev = false;
+let localhostDev = true;
 //如果是本地开发，就把localhostDev 设置成true，连接就是拼成‘http://localhost:8080/api/test’
 //生产环境，就把localhostDev 设置成false，连接就是拼成‘http://xxx.com/api/test’
-let _host= localhostDev ? 'http://112.74.181.229:7031/custAppApi' : 'http://custapi.shyj.cn/custAppApi/';
+let _host= localhostDev ? 'http://112.74.181.229:7031/custAppApi' : 'https://custapi.shyj.cn/custAppApi/';
 
 let URL = {
 	// 首页
