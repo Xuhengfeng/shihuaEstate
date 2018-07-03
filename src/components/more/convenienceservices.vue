9cfcb4f5e587797df3a42ef79521c8fc0e9efc38
xuhengfeng <564297479@qq.com>
2018-07-03 15:49:57 +0800

    update

--- src/components/more/convenienceservices.vue
+++ src/components/more/convenienceservices.vue
@@ -6,8 +6,31 @@
       <img src="../../imgs/more/bianming.png">
     </div>
     <div class="main">
-        <div></div>
-        <section><img src="../../imgs/more/bianming2.png"></section>
+        <section>
+          <div class="title">
+            <div class="fl line"></div>
+            <h4>便民类目</h4>
+            <div class="fr line"></div>
+          </div>
+          <div class="bgserver"><img src="../../imgs/more/bianming2.png"></div>
+        </section>
+
+        <section>
+          <div class="title">
+            <div class="fl line"></div>
+            <h4>便民宗旨</h4>
+            <div class="fr line"></div>
+          </div>
+          <div class="view container">
+            <img src="../../imgs/more/bianming3.png">
+            <div class="colorBox">
+              <p>凡是有世华易居门店的地方，您只需推门即可免费获得以下帮助：
+                便民饮水服务、应急卫生间、应急雨具、应急充电、复印、上网、市话等……</p>
+              <p>世华易居能为您提供的服务当然不只这些，每一位有困难的人，只要进入世华易居门店求助，均可获得世华人无偿的爱心服务，世华人将尽自己最大努力为您提供帮助。</p>
+              <p>世华易居的门店建设理念是，每一个世华易居门店，都是一个爱心小站，是一个正能量传播源头！世华易居希望用自己的真诚为身边的人带来方便，不再为意外的小事烦恼，并为此坚持不懈地努力！</p>     
+            </div>
+          </div>
+        </section>
     </div>
 	</div>
 </template>
@@ -25,9 +48,8 @@ export default {
 <style lang="less" scoped>
 .header {
     width: 100%;
-    height: 500px;
+    height: 660px;
     position: relative;
-    overflow: hidden;
     img{
         width: 1920px;
         position: absolute;
@@ -39,7 +61,66 @@ export default {
 .main{
   overflow: hidden;
 }
-.main section{
+.main .title{
+  width: 750px;
+  margin: 94px auto 112px;
+  height: 40px;
+  line-height: 40px;
   text-align: center;
+  display: flex;
+  flex-flow:  row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 40px;
+  color: #222222;
+}
+.main section:nth-of-type(1){
+  overflow: hidden;
+  background: #ffffff;
+  height: 695px;
+}
+.main section:nth-of-type(2){
+  overflow: hidden;
+  background: #f5f5f5;
+  height: 870px;
+}
+.main .title h4::after,
+.main .title h4::before{
+  content: '';
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: red;
+  transform: rotate(-45deg);
+  margin: 8px 10px;
+}
+.main .title .line{
+  height: 1px;
+  width: 206px;
+  background: #dddddd;
+}
+.main .bgserver{
+  text-align: center;
+}
+.view{
+  position: relative;
+}
+.colorBox{
+  position: absolute;
+  right: 0;
+  top: 40px;
+  background: #a6865f;
+  width: 667px;
+  height: 493px;
+  box-sizing: border-box;
+  padding: 20px;
+}
+.colorBox p{
+  line-height: 1.5;
+  text-indent: 20px;
+  text-align: justify;
+  color: #ffffff;
+  margin-bottom: 30px;
+  font-size: 20px;
 }
 </style>
